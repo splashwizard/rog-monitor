28a5cf6f9af1b6c5fa98b6a85f086a3f1ee34e6a
splashwizard <splashwizard9@gmail.com>
2021-09-23 10:55:47 -0500

    messagingSenderId

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const config = {
   projectId: "rog-2-0", //
   apiKey: "AIzaSyCY1oTVrozQfDrCG1k-b3Q4Iw5iWSF_LIM", //
   appId: "1:153344187169:web:64dd90de0f9831cc643c60", //
-  messagingSenderId: "153344187169"
+  messagingSenderId: "153344187169" //
 };
 
 const rrfConfig = {
