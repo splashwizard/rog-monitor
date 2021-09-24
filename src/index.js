ff2a61e9cb53b50e015222044e5395215decfb02
splashwizard <splashwizard9@gmail.com>
2021-09-24 09:45:10 -0500

    projectId

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { message } from 'antd';
 
 const config = {
-  projectId: "rog-2-0", //
+  projectId: "rog-2-0",
   apiKey: "AIzaSyCY1oTVrozQfDrCG1k-b3Q4Iw5iWSF_LIM", //
   appId: "1:153344187169:web:64dd90de0f9831cc643c60", //
   messagingSenderId: "153344187169" //
