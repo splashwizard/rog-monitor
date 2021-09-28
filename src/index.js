c445e782f760b523fe920ab06d66fc3f50b28119
splashwizard <splashwizard9@gmail.com>
2021-09-28 09:54:30 -0500

    apiKey

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { message } from 'antd';
 
 const config = {
   projectId: "rog-2-0",
-  apiKey: "AIzaSyCY1oTVrozQfDrCG1k-b3Q4Iw5iWSF_LIM", //
+  apiKey: "AIzaSyCY1oTVrozQfDrCG1k-b3Q4Iw5iWSF_LIM",
   appId: "1:153344187169:web:64dd90de0f9831cc643c60", //
   messagingSenderId: "153344187169" //
 };
